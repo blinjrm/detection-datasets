70a21e30148d228603b68091c15b112979971867
Jerome Blin <jerome@grover.com>
2022-08-24 23:26:48 +0200

    feat: add from_hf_dataset method in Dataset.

--- src/detection_datasets/writers/base.py
+++ src/detection_datasets/writers/base.py
@@ -3,10 +3,10 @@ import shutil
 from abc import ABC, abstractmethod
 from typing import List
 
-import wandb
-
 from detection_datasets.utils import Dataset
 from detection_datasets.utils.enums import Destinations
+
+# import wandb
 
 
 class BaseWriter(ABC):
@@ -55,45 +55,45 @@ class BaseWriter(ABC):
     def upload_to_wandb(self) -> None:
         """Uploads the dataset to W&B artifacts."""
 
-        try:
-            run = wandb.init(
-                project="detection-dataset",
-                settings=wandb.Settings(start_method="fork"),
-            )
+    #     try:
+    #         run = wandb.init(
+    #             project="detection-dataset",
+    #             settings=wandb.Settings(start_method="fork"),
+    #         )
 
-            # Log the entire dataset
-            artifact = wandb.Artifact(
-                self.name,
-                type="dataset",
-                metadata={
-                    "format": self.format,
-                    "n_images": self.n_images,
-                    "n_classes": self.n_classes,
-                    "class_names": self.class_names,
-                    "split_proportions": self.split_proportions,
-                },
-            )
-            artifact.add_dir(self.dataset_dir)
+    #         # Log the entire dataset
+    #         artifact = wandb.Artifact(
+    #             self.name,
+    #             type="dataset",
+    #             metadata={
+    #                 "format": self.format,
+    #                 "n_images": self.n_images,
+    #                 "n_classes": self.n_classes,
+    #                 "class_names": self.class_names,
+    #                 "split_proportions": self.split_proportions,
+    #             },
+    #         )
+    #         artifact.add_dir(self.dataset_dir)
 
-            # Log a table for dataset visualization
-            # table = self._make_wandb_table()
-            # artifact.add_file(table, name='table.csv')
+    #         # Log a table for dataset visualization
+    #         # table = self._make_wandb_table()
+    #         # artifact.add_file(table, name='table.csv')
 
-            # Upload to W&B
-            run.log_artifact(artifact, aliases=[self.format, str(self.n_images)])
+    #         # Upload to W&B
+    #         run.log_artifact(artifact, aliases=[self.format, str(self.n_images)])
 
-        except Exception as e:
-            self._delete_local_dataset()
-            raise e
+    #     except Exception as e:
+    #         self._delete_local_dataset()
+    #         raise e
 
-        # Delete local dataset
-        if Destinations.LOCAL_DISK not in self.destinations:
-            self._delete_local_dataset()
+    #     # Delete local dataset
+    #     if Destinations.LOCAL_DISK not in self.destinations:
+    #         self._delete_local_dataset()
 
-    def _make_wandb_table(self) -> wandb.Table:
-        """Creates a W&B table containing the dataset."""
+    # def _make_wandb_table(self) -> wandb.Table:
+    #     """Creates a W&B table containing the dataset."""
 
-        # data = self.data.copy()
+    #     # data = self.data.copy()
 
     def _delete_local_dataset(self) -> None:
         """Deletes the local dataset."""
