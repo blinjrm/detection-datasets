67ee06f0d2bc9e1b847b335343a6bb978f210e54
Jerome Blin <blinjrm@gmail.com>
2023-10-11 10:28:38 +0200

    build: bump dependencies

--- src/detection_datasets/__init__.py
+++ src/detection_datasets/__init__.py
@@ -2,6 +2,6 @@ from detection_datasets.bbox import Bbox
 from detection_datasets.detection_dataset import DetectionDataset
 from detection_datasets.utils.hub import available_in_hub
 
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 
 __all__ = [Bbox, DetectionDataset, available_in_hub]
